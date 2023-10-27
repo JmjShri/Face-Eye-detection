6b338a4c7d2202961bd7c035d03011d8e3bb328f
JmjShri <janmejayshrimali19@gmail.com>
2023-10-27 13:27:22 +0530

    initial commit

---
+++ face_eye_webcam.py
@@ -0,0 +1,30 @@
+
+import cv2
+cap=cv2.VideoCapture(0)
+fourcc=cv2.VideoWriter_fourcc(*'XVID')
+out=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+
+while cap.isOpened():
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    
+    for (x, y , w ,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey ,ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 5)
+
+    # Display the output
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
